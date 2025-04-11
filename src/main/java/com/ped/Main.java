4fcd7dcab9b8fd0861ee1a847339602b6332d175
Lucas Santos <kelytonlucas@gmail.com>
2025-04-11 20:45:24 -0300

    feat: starting config for object candidato

--- src/main/java/com/ped/Main.java
+++ src/main/java/com/ped/Main.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import com.ped.estruturaPdf.Candidato;
+
 public class Main {
     public static void main(String[] args) {
         try {
@@ -24,11 +26,25 @@ public class Main {
                 // .split("\\s+") dividi a linha em pedaços
                 // (\\s+) divide onde tiver 1 ou mais espaços/tabs
 
-                if (colunas.length >= 6) { // deve ter pelo menos 6 colunas
+                if (colunas.length >= 8) { // deve ter pelo menos 6 colunas
                     String pontos = colunas[colunas.length - 1]; // última coluna
                     String esp = colunas[colunas.length - 2];
-                    String dout = colunas[colunas.length - 3];
-                    System.out.println("DOUT " + dout + " ESP " + esp + " PONTOS: " + pontos);
+                    String mest = colunas[colunas.length - 3];
+                    String dout = colunas[colunas.length - 4];
+                    String firstName = colunas[1];
+                    String secondName = colunas[2];
+                    String lastName = colunas[3];
+                    String id = colunas[0];
+
+                    String name = firstName + secondName + lastName;
+
+                    System.out.println("id: " + id + " nome: " + name + " doutorado: " + dout + " mestrado: " + mest
+                            + " especializacao: " + esp + " total: " + pontos);
+                    /*
+                     * Candidato candidato = new Candidato(Integer.parseInt(id), name,
+                     * Float.parseFloat(dout), Float.parseFloat(mest), Float.parseFloat(esp),
+                     * Float.parseFloat(pontos));
+                     */
                 }
             }
 
